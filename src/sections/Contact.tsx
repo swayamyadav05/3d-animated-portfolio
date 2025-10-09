1d3632206379f68176fdb3ef67bf3c95c91a7d76
Mangloo69 <yadav69.swayam@gmail.com>
2025-10-10 00:46:09 +0545

    Added contact with emailJs

---
+++ src/sections/Contact.tsx
@@ -0,0 +1,133 @@
+import TitleHeader from "../components/TitleHeader";
+import ContactExperience from "../components/ContactExperience";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      if (!formRef.current) {
+        throw new Error("Form reference is null");
+      }
+      await emailjs.sendForm(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        formRef.current,
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      );
+
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      console.log("EMAILJS ERROR,", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <section id="contact" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="Get In Touch With Me"
+          sub="📧 Contact Information"
+        />
+
+        <div className="grid-12-cols mt-16">
+          {/* Left side - Contact Form */}
+          <div className="xl:col-span-5">
+            <div className="flex-center card-border rounded-xl p-10">
+              <form
+                onSubmit={handleSubmit}
+                className="w-full h-full flex flex-col gap-7"
+                ref={formRef}>
+                <div>
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Your Name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+
+                <div>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="your.email@example.com"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+
+                <div>
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows={5}
+                    placeholder="Your message here..."
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+
+                <button type="submit" disabled={loading}>
+                  <div className="cta-button group">
+                    <div className="bg-circle" />
+                    <p className="text">
+                      {loading ? "Sending..." : "Send Message"}
+                    </p>
+                    <div className="arrow-wrapper">
+                      <img src="/images/arrow-down.svg" alt="arrow" />
+                    </div>
+                  </div>
+                </button>
+              </form>
+            </div>
+          </div>
+
+          {/* Right side - 3D Experience */}
+          <div className="xl:col-span-7 min-h-96">
+            <div className="w-full h-full bg-[#cd7c2e] hover:cursor-grab rounded-3xl overflow-hidden">
+              <ContactExperience />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
