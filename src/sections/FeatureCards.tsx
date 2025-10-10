cfc96cce47d0ca752810e2c94228d986f0510fba
Mangloo69 <yadav69.swayam@gmail.com>
2025-10-11 01:55:46 +0545

    Added favicon and hover effect on feature cards

--- src/sections/FeatureCards.tsx
+++ src/sections/FeatureCards.tsx
@@ -7,7 +7,7 @@ const FeatureCards = () => {
         {abilities.map(({ imgPath, title, desc }) => (
           <div
             key={title}
-            className="card-border rounded-xl p-8 flex flex-col gap-4">
+            className="feature-card card-border rounded-xl p-8 flex flex-col gap-4">
             <div className="size-14 flex items-center justify-center rounded-full">
               <img
                 src={imgPath}
