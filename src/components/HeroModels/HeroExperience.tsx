e9a56a0900247fae6f274b7012547033f0136274
Swayam Yadav <97746128+swayamyadav05@users.noreply.github.com>
2026-02-07 04:51:39 +0530

    heroExperience added suspense

--- src/components/HeroModels/HeroExperience.tsx
+++ src/components/HeroModels/HeroExperience.tsx
@@ -4,6 +4,7 @@ import { useMediaQuery } from "react-responsive";
 import { Room } from "./Room";
 import HeroLights from "./HeroLights";
 import Particles from "./Particles";
+import { Suspense } from "react";
 
 const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
@@ -11,6 +12,7 @@ const HeroExperience = () => {
 
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+      <ambientLight intensity={0.2} color="#1a1a40" />
       <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
@@ -20,14 +22,16 @@ const HeroExperience = () => {
         maxPolarAngle={Math.PI / 2}
       />
 
-      <HeroLights />
-      <Particles />
-      <group
-        scale={isMobile ? 0.7 : 1}
-        position={[0, -3.5, 0]}
-        rotation={[0, -Math.PI / 4, 0]}>
-        <Room />
-      </group>
+      <Suspense fallback={null}>
+        <HeroLights />
+        <Particles />
+        <group
+          scale={isMobile ? 0.7 : 1}
+          position={[0, -3.5, 0]}
+          rotation={[0, -Math.PI / 4, 0]}>
+          <Room />
+        </group>
+      </Suspense>
     </Canvas>
   );
 };
